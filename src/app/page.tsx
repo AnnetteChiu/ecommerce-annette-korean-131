96bfef0942cc9de2fa9ef6adc1fbdfcc6984dca7
Annette Li-An Chiu <lian600058@gmail.com>
2025-06-30 08:30:15 +0000

    https://i4.codibook.net/files/1982120613804/cfee74844ee74050/1877193488.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,12 @@ export default function Home() {
         data-ai-hint="fashion collection"
       />
 
+      <ImageBanner 
+        src="https://i4.codibook.net/files/1982120613804/cfee74844ee74050/1877193488.jpg"
+        alt="Stylish outfit banner"
+        data-ai-hint="fashion style"
+      />
+
       <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-12">
         <main className="lg:col-span-3 space-y-16 lg:order-last">
           <BrandWeek />
