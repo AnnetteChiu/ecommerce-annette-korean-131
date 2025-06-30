40ce848a31e746b8d58be8283d102b868f3b936a
Annette Li-An Chiu <lian600058@gmail.com>
2025-06-30 08:13:36 +0000

    https://i4.codibook.net/files/1982120751817/d87a70fe9b9ff7/1124494606.jp

--- src/components/image-banner.tsx
+++ src/components/image-banner.tsx
@@ -5,7 +5,7 @@ export function ImageBanner() {
     <section>
       <div className="relative aspect-[3/1] w-full overflow-hidden rounded-lg shadow-lg">
         <Image
-          src="https://placehold.co/1200x400.png"
+          src="https://i4.codibook.net/files/1982120751817/d87a70fe9b9ff7/1124494606.jpg"
           alt="New Collection Banner"
           fill
           className="object-cover"
